3cc410b7a1040803dbee3c3b9f941cb2de7ba9d4
José Conejeros <jdconejeros@uc.cl>
2024-05-17 18:26:37 -0400

    upgrade function append data

--- Code/5.0 Append_birth_temp.R.R
+++ Code/5.0 Append_birth_temp.R.R
@@ -4,7 +4,7 @@
 rm(list=ls())
 source("Code/0.2 Settings.R")
 
-# Save results
+## Function append and save results ----
 load_and_extract_df <- function(file_path) {
   e <- new.env()  # Crea un nuevo entorno para cargar los datos
   load(file_path, envir = e)  # Carga el archivo en el entorno nuevo
@@ -13,13 +13,17 @@ load_and_extract_df <- function(file_path) {
   return(df)
 }
 
+
+## Temp Data ----
+
 path_to_files <- "Data/Output/parts_data_final_temp"  # Asegúrate de que este sea el path correcto a tu carpeta
 file_list <- list.files(path = path_to_files, pattern = "*.RDS", full.names = TRUE)
 births_weeks_temp <- map_df(file_list, load_and_extract_df)
 
 save(births_weeks_temp, file=paste0("Data/Output/", "births_2011_2020_weeks_temp", ".RData"))
-#save(births_weeks_hum, file=paste0("Data/Output/", "births_2011_2020_weeks_hum", ".RData"))
 
+
+## Hum Data ----
 
 path_to_files <- "Data/Output/parts_data_final_hum"  # Asegúrate de que este sea el path correcto a tu carpeta
 file_list <- list.files(path = path_to_files, pattern = "*.RDS", full.names = TRUE)
